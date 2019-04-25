f4aa0f82a045f92f6ffc51e7920d06ad769fce3b
Alessandra Fernandes <alej.rbd83@gmail.com>
2019-04-25 18:10:42 -0300

    modifying styles and adding comments to project

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import Title from './Title';
-// import SlideCards from './SlideCards';
-// import CustomValues from './CustomValues';
-import Card from './Card';
+import MuralSizeBox from './MuralSizeBox';
 import CustomValues from './CustomValues';
 
-let cards = [
+// Defino un array de objetos que va a contener los datos de cada uno de los elementos (size_boxes)
+let size_boxes = [
   {
     size: 'default',
     name: 'Default',
@@ -45,6 +43,9 @@ let cards = [
 
 ];
 
+// Creo la clase que extiende de componente con la estrcuctura standar donde defino el estado de mi aplicación
+// en dicho estado guardo el size que va a estar variando por cada elemento seleccionado.
+// tambien guardo los valores que tendrá el box custom tanto su width como height, asi se validan posteriormente
 class Form extends Component {
   constructor(props) {
     super(props);
@@ -56,68 +57,87 @@ class Form extends Component {
   }
 
   // recibo el valor de la funcion handleChange (en este caso el size que es lo que necesito para cambiar mi estado)
+  // una vez que selecciono algun box en particular, de esta manera puedo realizar una funcionalidad distinta para cada uno
   onSizeChange = (size) => {
     this.setState({
       size: size
     });
   };
 
+  // seteo el valor en el estado de customWidth cada vez que el usuario ingresa un cambio en el input
   onCustomWidthChange = (customWidth) => {
     this.setState({
       customWidth: customWidth
     });
   };
 
+  // seteo el valor en el estado de customHeight cada vez que el usuario ingresa un cambio en el input
   onCustomHeightChange = (customHeight) => {
     this.setState({
       customHeight: customHeight
     });
   };
 
+  // con esta función evito que se produzca el efecto secundario del evento predeterminado. En este caso el evento submit
+  // recargaría la página al clickear en el botón que tiene ese evento, esto evita que eso suceda.
   onSubmit = (event) => {
     event.preventDefault()
-    if(this.state.size === 'custom') {
-      if(this.state.customWidth < 3000 || this.state.customWidth > 60000) {
-        alert('El ancho debe ser mayor a 3000 y menor a 60000')
-      }
-      if(this.state.customHeight < 3000 || this.state.customHeight > 60000) {
-        alert('El alto debe ser mayor a 3000 y menor a 60000')
-      }
-    }
   }
 
+  // renderizo todo lo que deseo mostrar
   render() {
-    console.log(this.state.size)
+    // guardo cada una de las propiedades de mi estado en variables para que sea mas fácil de utilizar
+    const { size, customWidth, customHeight} = this.state
     return (
+      // etiqueta form con el evento onSubmit que llama a la función onSubmit definida arriba
       <form onSubmit={this.onSubmit}>
-      <Title />
-      <p>MURAL SIZE</p>
+      <div className="title">
+        <p>MURAL</p>
+        <h1>UI CHALLENGE</h1>
+        <hr></hr>
+      </div>
+      <p className="mural-size-name">MURAL SIZE</p>
       <div className="mural-size-selector">
-        {cards.map((card, index) => {
+      {/* con map recorro cada uno de los elementos de mi array y los retorno en el componente MuralSizeBox, esto devolverá
+      un box con todas las propiedades definidas en el map por cada elemento, es decir, en este caso 6 boxes, en la
+      propiedad selected comparo si el valor size es igual al que esta guardado en el estado, de esta manera me devolverá
+      un booleano que posteriormente lo uso para ponerle una deterinada clase de css. En la propiedad onChange recibo la función
+      onSizeChange definida arriba */}
+        {size_boxes.map((box, index) => {
          return (
-          <Card
+          <MuralSizeBox
             key={index}
-            mySize={card.size}
-            name={card.name}
-            text={card.text}
-            proportion={card.proportion}
-            selected={card.size === this.state.size}
+            mySize={box.size}
+            name={box.name}
+            text={box.text}
+            proportion={box.proportion}
+            selected={box.size === size}
             onChange={this.onSizeChange}
             />
          )
         })}
       </div>
-      {this.state.size === 'custom' ? (
+      {/* con esta comparacion puedo saber si el box seleccionado es el box custom y desencadenar una funcionalidad única que tiene este box:
+      dos inputs con valores personalizados por el usuario los cuales se setearan en el estado gracias a las funciones onCustomWidthChange
+      y onCustomHeightChange, si no es custom el box seleccionado no se desencadena nada */}
+      {size === 'custom' ? (
       <CustomValues
         onCustomWidthChange={this.onCustomWidthChange}
         onCustomHeightChange={this.onCustomHeightChange}
-        customWidth={this.state.customWidth}
-        customHeight={this.state.customHeight}
+        customWidth={customWidth}
+        customHeight={customHeight}
       />
       ) : null}
-      <button type="submit" disabled={this.state.size === ''}>
+      <div className="mural-button">
+      {/* aca defino el boton que da por finalizado el formulario, este boton se habilita solo si recibe valores válidos, es decir,
+      hay algún elemento seleccionado o los valores personalizados de custom estan entre 3000 y 60000 */}
+      <button
+        type="submit"
+        disabled={size === '' || customWidth <3000
+        || customWidth >60000 || customHeight <3000 || customHeight >60000}>
         DONE
       </button>
+      </div>
 
       </form>
 
--- src/components/MuralSizeBox.js
+++ src/components/MuralSizeBox.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
 import CustomImg from '../static/images/custom-size-icon.svg';
 
-class Card extends Component {
-
-  // onClickCard = () => {
-  //   console.log('llegue')
-  // };
+class MuralSizeBox extends Component {
 
   render() {
+    // recibo las propiedades desde el componente form a traves de props para utilizarlas y mostralas en cada box
     const { selected, mySize, name, text, proportion } = this.props
-    console.log(selected)
     return (
       <span>
         <input type="radio" name="mural-size" onChange={this.handleChange} id={mySize} />
+        {/* si la propiedad selected es true (booleano que llega por props) entonces aplicara una clase css con ese nombre, sino no hará nada */}
         <label className={'mural-size-item ' + (selected ? 'selected' : '')} htmlFor={mySize}>
         <div className={'mural-size ' + mySize}>
+        {/* si el dato size guardado en el array coincide con el string "custom" pintará un box especifico con una imagen diferente al resto, sino mostrará
+      el texto de cada box */}
         { mySize === 'custom'
               ? <img src={CustomImg} alt="arrow icon"/>
               : <p>{text}</p>}
             </div>
-            <p className="p">{name}</p>
+            <p className="name">{name}</p>
             <span>{proportion}</span>
           </label>
       </span>
     );
   }
 
+  // función que recibe a través de props el size del estado y con el evento onChange lo devuelve para poder cambiar su valor
   handleChange = () => {
     const { mySize } = this.props;
     this.props.onChange(mySize);
@@ -34,7 +34,7 @@ class Card extends Component {
 
 }
 
-export default Card;
+export default MuralSizeBox;
 
 
 
